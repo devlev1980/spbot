479bd845d5cf54d0e46ba37aa5500d0700d7188e
Yevgeny <devlev1980@gmail.com>
2018-11-29 12:07:00 +0200

    add getMovieDetail + findMovie

--- app.js
+++ app.js
@@ -107,6 +107,71 @@ function processPostback(event) {
     });
   }
 }
+function getMovieDetail(userId, field) {
+  Movie.findOne({user_id: userId}, function(err, movie) {
+    if(err) {
+      sendMessage(userId, {text: "Something went wrong. Try again"});
+    } else {
+      sendMessage(userId, {text: movie[field]});
+    }
+  });
+}
+function findMovie(userId, movieTitle) {
+  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
+    if (!error &amp;&amp; response.statusCode === 200) {
+      var movieObj = JSON.parse(body);
+      if (movieObj.Response === "True") {
+        var query = {user_id: userId};
+        var update = {
+          user_id: userId,
+          title: movieObj.Title,
+          plot: movieObj.Plot,
+          date: movieObj.Released,
+          runtime: movieObj.Runtime,
+          director: movieObj.Director,
+          cast: movieObj.Actors,
+          rating: movieObj.imdbRating,
+          poster_url:movieObj.Poster
+        };
+        var options = {upsert: true};
+        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+          if (err) {
+            console.log("Database error: " + err);
+          } else {
+            message = {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: movieObj.Title,
+                    subtitle: "Is this the movie you are looking for?",
+                    image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                    buttons: [{
+                      type: "postback",
+                      title: "Yes",
+                      payload: "Correct"
+                    }, {
+                      type: "postback",
+                      title: "No",
+                      payload: "Incorrect"
+                    }]
+                  }]
+                }
+              }
+            };
+            sendMessage(userId, message);
+          }
+        });
+      } else {
+        console.log(movieObj.Error);
+        sendMessage(userId, {text: movieObj.Error});
+      }
+    } else {
+      sendMessage(userId, {text: "Something went wrong. Try again."});
+    }
+  });
+}
 
 // sends message to user
 function sendMessage(recipientId, message) {
